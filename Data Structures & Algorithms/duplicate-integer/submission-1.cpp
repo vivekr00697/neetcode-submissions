b4faa781471a432425fddceb86c920d0d5842a89
vivekr00697 <vivekremesh006@gmail.com>
2026-07-25 15:52:09 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> hash;
+        for(int i=0;i<nums.size();i++){
+            if(hash.count(nums[i])){
+                return true;
+            }
+            hash.insert(nums[i]);
+        }
+        return false;
+    }
+};
